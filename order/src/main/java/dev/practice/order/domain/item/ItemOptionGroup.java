0a7bdf92df985e0a18161b68b657529abf48dec6
xodn5235 <sssunggure@naver.com>
2022-03-09 23:11:06 +0900

    [CREATE] Item Domain Entity, 연관관계 구현

---
+++ order/src/main/java/dev/practice/order/domain/item/ItemOptionGroup.java
@@ -0,0 +1,44 @@
+package dev.practice.order.domain.item;
+
+import com.google.common.collect.Lists;
+import dev.practice.order.common.exception.InvalidParamException;
+import dev.practice.order.domain.AbstractEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "item_option_groups")
+public class ItemOptionGroup extends AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+    private Integer ordering;
+    private String itemOptionGroupName;
+
+    @Builder
+    public ItemOptionGroup(Item item, Integer ordering, String itemOptionGroupName) {
+        if(item == null) throw new InvalidParamException("ItemOptionGroup.item");
+        if(ordering == null) throw new InvalidParamException("ItemOptionGroup.ordering");
+        if(StringUtils.isEmpty(itemOptionGroupName)) throw new InvalidParamException("itemOptionGroup.itemOptionGroupName");
+
+        this.item = item;
+        this.ordering = ordering;
+        this.itemOptionGroupName = itemOptionGroupName;
+    }
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST, mappedBy = "itemOptionGroup")
+    private List<ItemOption> itemOptionList = Lists.newArrayList();
+
+}
